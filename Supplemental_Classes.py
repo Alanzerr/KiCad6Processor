574e1e0650922357172570855b314ed0c203fd17
Alanzerr <47159990+Alanzerr@users.noreply.github.com>
2022-12-06 18:10:20 +0000

    Can 1. output a kiCad file, 2. update the libray 3. add the footprint and models correctly 4. add the model of the original board
    Can't
    1. draw silkscreen for connectors
    2. generate the symbol

---
+++ Supplemental_Classes.py
@@ -0,0 +1,78 @@
+#  ===================================================================
+#  Source File Name : Supplemental_Classes.py
+#  Purpose          : Supplemental classes for various things.
+#  Author           : Alan Milne
+#  #
+#  This is copyright (C) 2022 to Alan Milne
+#  ===================================================================
+
+# from typing import Optional, List
+# from os import path
+
+# from kiutils.utils.strings import dequote
+# from kiutils.utils import sexpr
+from kiutils.footprint import *
+
+
+# ==================================================================================================================
+@dataclass
+class LongAxis:
+    x_axis: bool = False
+    y_axis: bool = False
+
+
+# ==================================================================================================================
+@dataclass
+class Circle:
+    center: Position = Position()
+    radius: float    = 0.0
+
+
+# ==================================================================================================================
+@dataclass
+class Coordinate2D:
+    X: float = 0.0
+    Y: float = 0.0
+
+
+# ==================================================================================================================
+@dataclass
+class Component:
+    pads                       = []
+    models                     = []
+
+
+# ==================================================================================================================
+@dataclass
+class AuxData:
+    # This is to supplement footprint with extra data to make it easier to use. Built on top of Footprint.
+
+    # Is the footprint origin the center or Pin 1
+    center_origin:   bool         = False
+
+    # Offset to Center from pin 1.
+    center:          Coordinate2D = Coordinate2D()
+
+    # used to determine the long axis so that we know what way to flip
+    longaxis_X:      bool         = True
+
+    flip:            bool         = False
+    header:          bool         = True
+    pin_pitch:       float        = 0.0
+    layer:           str          = ""
+    id:              str          = ""
+    type:            str          = ""
+
+    # Board offset
+    centroid_offset: Position     = Position()
+
+    # Board flip
+    board_flip:      bool         = False
+
+
+# ==================================================================================================================
+@dataclass
+class Components:
+    component_data: list[Component] = field(default_factory=list)
+
+    menu_data:      list[str]       = field(default_factory=list)
